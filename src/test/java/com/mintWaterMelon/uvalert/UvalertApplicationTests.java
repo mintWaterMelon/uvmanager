218890af9cfe351fabb9762d6937c47c7ec2c9e3
mintWaterMelon <134702226+lee990324@users.noreply.github.com>
2025-09-08 10:52:50 +0900

    INIT: Spring Boot 프로젝트 생성

---
+++ src/test/java/com/mintWaterMelon/uvalert/UvalertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mintWaterMelon.uvalert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UvalertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
