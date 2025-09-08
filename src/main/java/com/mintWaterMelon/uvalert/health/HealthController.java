85315e6467d355a84615a7e96f6bebfd49409925
mintWaterMelon <134702226+lee990324@users.noreply.github.com>
2025-09-08 11:56:30 +0900

    REFACTOR: DTO로 API 응답 구조 정리하기

--- src/main/java/com/mintWaterMelon/uvalert/health/HealthController.java
+++ src/main/java/com/mintWaterMelon/uvalert/health/HealthController.java
@@ -4,8 +4,6 @@ import com.mintWaterMelon.uvalert.health.dto.HealthResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
-
 @RestController
 public class HealthController {
 
