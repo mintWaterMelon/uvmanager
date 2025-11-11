be9fc16b966e5c92003399564b8d57c4db395625
mintWaterMelon <134702226+lee990324@users.noreply.github.com>
2025-11-11 15:51:09 +0900

    TEST: 테스트 서버 생성

--- backend/src/test/java/com/mintWaterMelon/uvalert/UvalertApplicationTests.java
+++ backend/src/test/java/com/mintWaterMelon/uvalert/UvalertApplicationTests.java
@@ -2,7 +2,9 @@ package com.mintWaterMelon.uvalert;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class UvalertApplicationTests {
 
